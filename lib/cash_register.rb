940fe59868b97a7f3e6fc60c1283363aa4ea8690
Evan Jeffries <ejeffries14@gmail.com>
2019-09-09 04:22:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
-
+class CashRegister
+  attr_accessor :total
+  
+  new = 0
+  
+  def initialize(total, discount)
+    total = 0
+    @total = total
+  end
+  
+  def total
+    @total
+  end
+end
